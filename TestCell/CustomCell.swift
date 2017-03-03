f78661677b861042c115d6a064778eee3f5d74c6
matsuda <kosukematsuda@gmail.com>
2017-03-03 18:20:48 +0900

    update cell
    set preferredMaxLayoutWidth in awakeFromNib

--- TestCell/CustomCell.swift
+++ TestCell/CustomCell.swift
@@ -13,10 +13,23 @@ class CustomCell: UITableViewCell {
     @IBOutlet weak var label1: UILabel!
     @IBOutlet weak var label2: UILabel!
     @IBOutlet weak var label3: UILabel!
+    @IBOutlet weak var trailingConstraing: NSLayoutConstraint!
+    @IBOutlet weak var leadingConstraint: NSLayoutConstraint!
 
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+//        let screenWidth = UIScreen.main.bounds.width
+//        print(">>>>", bounds.width, " : ", screenWidth)
+//        if #available(iOS 9.0, *) {
+//        } else {
+//            if screenWidth != bounds.width && screenWidth > 400 {
+//                let constraintWidth = screenWidth - 33 - leadingConstraint.constant
+//                label1.preferredMaxLayoutWidth = constraintWidth
+//                label2.preferredMaxLayoutWidth = constraintWidth
+//                label3.preferredMaxLayoutWidth = constraintWidth
+//            }
+//        }
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -24,5 +37,15 @@ class CustomCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
-    
+
+//    override func layoutSubviews() {
+//        super.layoutSubviews()
+//        var width = UIScreen.main.bounds.width - 5
+//        width -= (33 + 94)
+//        let w = label1.frame.width
+//        print(">>>>>>>>>", w, " : ", width)
+//        label1.preferredMaxLayoutWidth = width
+//        label2.preferredMaxLayoutWidth = width
+//        label3.preferredMaxLayoutWidth = width
+//    }
 }
